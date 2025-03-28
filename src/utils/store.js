e8fb7abff346c93df3f58b3f83bae1e82e724b45
dash <dinhtiendat0201@gmail.com>
2025-03-28 23:10:29 +0700

    fix wrong dist folder

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,24 +1,18 @@
 import { load } from "@tauri-apps/plugin-store";
-// when using `"withGlobalTauri": true`, you may use
-// const { load } = window.__TAURI__.store;
 
-// Create a new store or load the existing one,
-// note that the options will be ignored if a `Store` with that path has already been created
-const store = await load("store.json", { autoSave: false });
-
-// Hàm lưu dữ liệu
 export async function setData(key, value) {
+  const store = await load("store.json", { autoSave: false });
   await store.set(key, value);
   await store.save(); // Lưu vào file
 }
 
-// Hàm lấy dữ liệu
 export async function getData(key) {
+  const store = await load("store.json", { autoSave: false });
   return await store.get(key);
 }
 
-// Hàm xóa dữ liệu
 export async function removeData(key) {
+  const store = await load("store.json", { autoSave: false });
   await store.delete(key);
   await store.save();
 }
